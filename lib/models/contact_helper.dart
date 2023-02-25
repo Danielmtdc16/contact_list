f8c344ebef411399509634b4531a2ecb0f819af9
Daniel <danielmtdc16@gmail.com>
2023-02-25 13:49:33 -0300

    Inicializando o banco de dados.

--- lib/models/contact_helper.dart
+++ lib/models/contact_helper.dart
@@ -1,5 +1,8 @@
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'dart:async';
 
+const String contactTable = "contactTable";
 const String idColumn = "idColumn";
 const String nameColumn = "nameColumn";
 const String emailColumn = "emailColumn";
@@ -8,7 +11,33 @@ const String imgColumn = "imgColumn";
 
 class ContactHelper{
 
+  static final ContactHelper _instance = ContactHelper.internal();
 
+  factory ContactHelper() => _instance;
+
+  ContactHelper.internal();
+
+  Database? _db;
+
+  Future<Database?> get db async {
+    if (_db != null){
+      return _db;
+    } else {
+      _db = await initDb();
+      return _db;
+    }
+  }
+
+  Future<Database> initDb() async {
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, "contacts.db");
+
+    return await openDatabase(path, version: 1, onCreate: (Database db, int newerVersion) async{
+      await db.execute(
+        "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT, $phoneColumn TEXT, $imgColumn TEXT)"
+      );
+    });
+  }
 
 }
 
@@ -39,7 +68,6 @@ class Contact {
     if (id != null){
       map[idColumn] = id;
     }
-
     return map;
   }
 }
